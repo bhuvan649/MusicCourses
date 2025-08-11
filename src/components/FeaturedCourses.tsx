276224ea1e7bd1293b130c6676797f393ca06b0f
bhuvan649 <vasulebhuvanesh@gmail.com>
2025-08-11 09:33:02 +0530

    Initial commit

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function FeaturedCourses() {
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+            <h2>FEATURED COURSES</h2>
+            <p>Learn with the Best</p>
+            
+            </div></div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  )
+}
+
+export default FeaturedCourses
